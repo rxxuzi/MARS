f20768daa8869074f6cc290d953a643c1abf32ca
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-05-02 00:30:37 +0900

    MARS

--- src/main/scala/XXX.java
+++
@@ -1,9 +0,0 @@
-public class XXX {
-    public static int xc = 0;
-    XXX() {
-        System.out.println("XXX" + xc);
-    }
-    public void add(int a , int b) {
-        System.out.println(a + b);
-    }
-}
